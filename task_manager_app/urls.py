f2f5a271e64bcd7f3184f5a0313c67a624fa36a4
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-23 02:49:02 +0300

    fix urls.py names

--- task_manager_app/urls.py
+++ task_manager_app/urls.py
@@ -26,28 +26,49 @@ app_name = "task_manager"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
     # Task URLs
-    path("task/list/", TaskListView.as_view(), name="task_list"),
-    path("task/create/", TaskCreateView.as_view(), name="task_create"),
-    path("task/update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
-    path("task/detail/<int:pk>/", TaskDetailView.as_view(), name="task_detail"),
-    path("task/delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
-
+    path("task/list/", TaskListView.as_view(), name="task-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/detail/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("task/delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
     # TaskType URLs
-    path('tasktypes/', TaskTypeListView.as_view(), name='task_type_list'),
-    path('tasktypes/create/', TaskTypeCreateView.as_view(), name='task_type_create'),
-    path('tasktypes/<int:pk>/update/', TaskTypeUpdateView.as_view(), name='task_type_update'),
-    path('tasktypes/<int:pk>/delete/', TaskTypeDeleteView.as_view(), name='task_type_delete'),
-
+    path("tasktypes/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("tasktypes/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
+    path(
+        "tasktypes/<int:pk>/update/",
+        TaskTypeUpdateView.as_view(),
+        name="task-type-update",
+    ),
+    path(
+        "tasktypes/<int:pk>/delete/",
+        TaskTypeDeleteView.as_view(),
+        name="task-type-delete",
+    ),
     # Position URLs
-    path('positions/', PositionListView.as_view(), name='position_list'),
-    path('positions/create/', PositionCreateView.as_view(), name='position_create'),
-    path('positions/<int:pk>/update/', PositionUpdateView.as_view(), name='position_update'),
-    path('positions/<int:pk>/delete/', PositionDeleteView.as_view(), name='position_delete'),
-
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("positions/create/", PositionCreateView.as_view(), name="position-create"),
+    path(
+        "positions/<int:pk>/update/",
+        PositionUpdateView.as_view(),
+        name="position-update",
+    ),
+    path(
+        "positions/<int:pk>/delete/",
+        PositionDeleteView.as_view(),
+        name="position-delete",
+    ),
     # Employee URLs
-    path('employees/', EmployeeListView.as_view(), name='employee_list'),
-    path('employees/create/', EmployeeCreateView.as_view(), name='employee_create'),
-    path('employees/<int:pk>/', EmployeeDetailView.as_view(), name='employee_detail'),
-    path('employees/<int:pk>/update/', EmployeeUpdateView.as_view(), name='employee_update'),
-    path('employees/<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee_delete'),
+    path("employees/", EmployeeListView.as_view(), name="employee-list"),
+    path("employees/create/", EmployeeCreateView.as_view(), name="employee-create"),
+    path("employees/<int:pk>/", EmployeeDetailView.as_view(), name="employee-detail"),
+    path(
+        "employees/<int:pk>/update/",
+        EmployeeUpdateView.as_view(),
+        name="employee-update",
+    ),
+    path(
+        "employees/<int:pk>/delete/",
+        EmployeeDeleteView.as_view(),
+        name="employee-delete",
+    ),
 ]
