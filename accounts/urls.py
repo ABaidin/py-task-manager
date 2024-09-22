b29bcd92d91840b468a3369941fdec68a70750a5
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-22 20:14:26 +0300

    add logout/ path to urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accounts.views import CustomLoginView, CustomRegisterView
+from accounts.views import CustomLoginView, CustomRegisterView, CustomLogoutView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("register/", CustomRegisterView.as_view(), name="register"),
+    path("logout/", CustomLogoutView.as_view(), name="logout"),
 ]
