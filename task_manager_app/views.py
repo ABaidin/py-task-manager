ecf11e32e125ad3b5c21d62ccbed39168ef46952
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-22 20:36:09 +0300

    create TaskListView

--- task_manager_app/views.py
+++ task_manager_app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
 
-# Create your views here.
+from task_manager_app.models import Task
+
+
+class TaskListView(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = "task_list.html"
+    paginate_by = 10
+
+    def get_queryset(self):
+        show_my_tasks = self.request.GET.get("show_my_tasks")
+
+        if show_my_tasks:
+            return Task.objects.filter(assigned_to=self.request.user)
+        else:
+            return Task.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["show_my_tasks"] = self.request.GET.get("show_my_tasks")
+        return context
