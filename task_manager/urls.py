ee7e7259c8d42c53a64275e107f2542e2553fa94
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-22 20:18:01 +0300

    add task_manager_app urls to project urls

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls", namespace="accounts")),
+    path("", include("task_manager_app.urls", namespace="tasks")),
 ]
