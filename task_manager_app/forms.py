cd723601023651b4c0b69c94b4752ad708333dac
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-22 21:09:19 +0300

    create TaskSearchForm

--- task_manager_app/forms.py
+++ task_manager_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from task_manager_app.models import Task
+from task_manager_app.models import Task, TaskType
 
 
 class TaskForm(forms.ModelForm):
@@ -12,3 +12,31 @@ class TaskForm(forms.ModelForm):
         widgets = {
             "assigned_to": forms.CheckboxSelectMultiple()
         }
+
+
+class TaskSearchForm(forms.Form):
+    name = forms.CharField(
+        max_length=63,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Search by name..."
+            }
+        )
+    )
+
+    task_type = forms.ModelChoiceField(
+        queryset=TaskType.objects.all(),
+        required=False,
+        label="",
+        widget=forms.Select(attrs={"class": "form-select"}),
+        empty_label="-- Filter by Task Type --"
+    )
+
+    show_my_tasks = forms.BooleanField(
+        required=False,
+        label="Show only my tasks",
+        widget=forms.CheckboxInput()
+    )
