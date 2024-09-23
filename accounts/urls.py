5f15e9abead30c996517f22bc632fd9f3e7046f0
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-09-23 03:33:31 +0300

    changed CustomLogoutView to default LogoutView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from accounts.views import CustomLoginView, CustomRegisterView, CustomLogoutView
+from accounts.views import CustomLoginView, CustomRegisterView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
     path("register/", CustomRegisterView.as_view(), name="register"),
-    path("logout/", CustomLogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
