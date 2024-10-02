5c87ee0707d7d4ffbbac0e13b78a2a07a6c6cb04
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-10-03 02:20:40 +0300

    add task-manager-otmm.onrender.com to ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "[::1]"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "task-manager-otmm.onrender.com"]
 
 # Application definition
 
